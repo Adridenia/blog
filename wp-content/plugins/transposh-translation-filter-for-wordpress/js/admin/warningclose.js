7a0d966b2b388b8524d47af7056049fa1cd182cf
Victor Hugo Valle Castillo <victouk@gmail.com>
2013-06-07 18:26:16 -0300

    adding translations

---
+++ wp-content/plugins/transposh-translation-filter-for-wordpress/js/admin/warningclose.js
@@ -0,0 +1,11 @@
+/*
+ * Transposh v0.9.3.1
+ * http://transposh.org/
+ *
+ * Copyright 2013, Team Transposh
+ * Licensed under the GPL Version 2 or higher.
+ * http://transposh.org/license
+ *
+ * Date: Thu, 09 May 2013 03:20:09 +0300
+ */
+(function(a){a(function(){a(".warning-close").click(function(){a(this).parents("div:first").hide();a.post(ajaxurl,{action:"tp_close_warning",id:a(this).attr("id")})})})})(jQuery);
---
+++ wp-content/plugins/transposh-translation-filter-for-wordpress/wp/transposh_ajax.php
@@ -0,0 +1,20 @@
+<?php
+
+/*
+ * Transposh v0.9.3.1
+ * http://transposh.org/
+ *
+ * Copyright 2013, Team Transposh
+ * Licensed under the GPL Version 2 or higher.
+ * http://transposh.org/license
+ *
+ * Date: Thu, 09 May 2013 03:20:09 +0300
+ */
+
+/*
+ * This file handles various AJAX needs of our plugin
+ */
+
+echo "This file was removed in 0.8.0 and is only kept for debugging";
+
+?>
